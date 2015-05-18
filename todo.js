bf5816cfd88f1f86aa60eb7f1483e9cffd9ff7fa
Raj Singh <raj@foreversingh.com>
2015-05-18 14:38:25 -0500

    initial js app walk-through from angular site

---
+++ todo.js
@@ -0,0 +1,7 @@
+angular.module('todoApp', [])
+	.controller('TodoListController', function(){
+		var todoList = this;
+		todoList.todos = [
+		{text:'learn angular', done:true},
+		{text:'build an angular app', done:false}];
+	})
